a2231ae8a55ef86e6cd4bdcba384d03c8fbd796f
Baptdcb <Baptiste.Do-Cabo@etu.univ-lyon1.fr>
2026-09-23 08:40:12 +0200

    Resolve merge conflict in Footer.jsx by removing redundant code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,6 @@ function LegalList({ lines }) {
 export default function Footer({ t }) {
   const [legalOpen, setLegalOpen] = useState(false)
   const { footer, legal } = t
-<<<<<<< HEAD
 
   useEffect(() => {
     const sync = () => {
@@ -27,9 +26,6 @@ export default function Footer({ t }) {
     return () => window.removeEventListener('hashchange', sync)
   }, [])
 
-=======
-  const navLabel = (id) => t.nav.links.find((l) => l.id === id)?.label
->>>>>>> main
   return (
     <footer className="footer">
       <div className="container">
